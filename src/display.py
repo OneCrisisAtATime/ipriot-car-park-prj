169e704380b6b13a976cacd212b00d96424610f0
J270113 <J270113@tafe.wa.edu.au>
2025-06-27 05:31:31 -0700

    test: ran display unit tests

--- src/display.py
+++ src/display.py
@@ -1,19 +1,19 @@
-DEFAULT_MESSAGE = ""
-DEFAULT_IS_ON = False
 
 class Display:
 
-    def __init__(self, id_=None, message="", is_on=False, car_park=None):
+    def __init__(self, id_, car_park, message="", is_on=False, ):
         self.id_ = id_
         self.message = message
         self.is_on = is_on
         self.car_park = car_park
 
+    def update(self, data):
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+
     def __str__(self):
         return f'Display {str(self.id_)}: {self.message}'
 
-    def update(self, data):
-        for key, value in data.items():
-            print(f'{key}: {value}')
 
-print(Display(2,"Welcome stranger!")) # test
+#print(Display(2, "random park","Welcome to the car park.")) # test
