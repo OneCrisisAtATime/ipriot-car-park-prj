679a327f3392d61c5b4a2d1860ec318e35510b72
J270113 <J270113@tafe.wa.edu.au>
2025-06-27 05:31:31 -0700

    test: ran display unit tests

--- src/car_park.py
+++ src/car_park.py
@@ -1,6 +1,8 @@
 import math
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime
 
 DEFAULT_LOCATION = "unknown"
 DEFAULT_CAPACITY = 1200
@@ -8,15 +10,19 @@ DEFAULT_TEMPERATURE = 25
 
 
 class CarPark:
-    def __init__(self, location, capacity, temperature,disabled_capacity=None, plates=None, sensors=None, displays=None):
+    def __init__(self, location, capacity, temperature, log_file='log.txt', plates=None, sensors=None, displays=None):
         self.location = location
         self.capacity = capacity
         self.temperature = temperature
         self.plates = plates or []
         self.sensors = sensors or []
         self.displays = displays or []
+        self.disabled_capacity = self.calculate_disabled_bays or 0
+        # convert file name to path and create it
+        self.log_file = Path(log_file)
+        if not self.log_file.exists():
+            self.log_file.touch()
 
-        self.disabled_capacity = self.calculate_disabled_bays() or 0
 
     def __str__(self):
         # return string containing car parks location and capacity
@@ -32,16 +38,24 @@ class CarPark:
         elif component is Display:
             self.displays.append(component)
 
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as file:
+            file.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+
     def add_car(self, plate):
         self.plates.append(plate)
         self.update_displays()
+
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         self.plates.remove(plate)
         self.update_displays()
 
+        self._log_car_activity(plate, "exited")
+
     def update_displays(self):
-        data = {"total available bays": self.available_bays, "available disabled bays": self.available_disability_bays} # , "temperature": self.temperature
+        data = {"total available bays": self.available_bays, "available disabled bays": self.available_disability_bays, "temperature": self.temperature}
 
         for display in self.displays:
             display.update(data)
@@ -54,6 +68,7 @@ class CarPark:
     def available_disability_bays(self):
             return max(0, self.disabled_capacity - len(self.plates))
 
+    @property
     def calculate_disabled_bays(self):
         """Assigns capacity of disabled parking bays based on the Australian Disability Network guidelines for accessible parking."""
         ### Source: https://australiandisabilitynetwork.org.au/DFD/dfd-06-05-car-parking.html ###
